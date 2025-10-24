5060259d9ab7363fac8cb1da7e3c8ba6b555255f
Said <said378175@gmail.com>
2025-10-24 12:55:26 +0300

    update security config

--- src/main/java/inaction/springsecurity/config/ProjectConfig.java
+++ src/main/java/inaction/springsecurity/config/ProjectConfig.java
@@ -2,6 +2,8 @@ package inaction.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -10,15 +12,16 @@ public class ProjectConfig {
 
     @Bean
     UserDetailsService userDetailsService(){
-        return new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("Saeed")
+                .password("1234")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
 
         }
 
 
-    @Bean
-    UserDetailsService duserDetailsService(){
-        return new InMemoryUserDetailsManager();
 
-    }
 
 }
