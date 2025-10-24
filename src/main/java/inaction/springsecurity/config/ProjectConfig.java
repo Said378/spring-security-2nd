19cf9fed383d294aa15994f32e5ec52e79f76247
Your Name <you@example.com>
2025-10-24 07:44:18 +0300

    init upload

---
+++ src/main/java/inaction/springsecurity/config/ProjectConfig.java
@@ -0,0 +1,17 @@
+package inaction.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        return new InMemoryUserDetailsManager();
+
+        }
+
+}
