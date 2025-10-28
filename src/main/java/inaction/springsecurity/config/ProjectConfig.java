55fa2aed6eea7d672437a263848074d30a71ddf3
Said Abdulkarim <said378157@gmail.com>
2025-10-28 20:51:26 +0300

    add custome auth provider

--- src/main/java/inaction/springsecurity/config/ProjectConfig.java
+++ src/main/java/inaction/springsecurity/config/ProjectConfig.java
@@ -2,24 +2,68 @@ package inaction.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class ProjectConfig {
 
+
+    private final CustomAuthenticationProvider authenticationProvider;
+    public ProjectConfig(CustomAuthenticationProvider authenticationProvider) {
+
+        this.authenticationProvider = authenticationProvider;
+    }
+
+//    @Bean
+//    UserDetailsService userDetailsService(){
+//
+//        UserDetails user = User.withUsername("Saeed")
+//                .password("1234")
+//                .authorities("read")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//
+//    }
+
+    /** NoOpPasswordEncoder treats password as plain text, it doesn't
+     * encrypt or hash them only compares the string using
+     *  equal method
+     *  **/
     @Bean
-    UserDetailsService userDetailsService(){
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 
-        UserDetails user = User.withUsername("Saeed")
+
+    /** to customize handling of authentication and authorization
+     * we need to define bean of type SecurityFilterChain
+     * **/
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+        http.authenticationProvider(authenticationProvider);
+
+        var user = User.withUsername("said")
                 .password("1234")
-                .authorities("read")
-                .build();
+                .authorities("read").build();
 
-        return new InMemoryUserDetailsManager(user);
+        var userDetailsService = new InMemoryUserDetailsManager(user);
+        http.userDetailsService(userDetailsService);
 
+        return http.build();
     }
 
 
